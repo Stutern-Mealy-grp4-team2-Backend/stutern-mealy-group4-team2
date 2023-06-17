ab64ceea9820ffe1614934c6384d0c1d5faef16d
Iupac120 <100804858+Iupac120@users.noreply.github.com>
2023-06-17 04:16:13 +0100

    added price

--- src/routers/cart.route.js
+++ src/routers/cart.route.js
@@ -2,8 +2,7 @@ import express from "express"
 const router = express.Router()
 import CartController from "../controllers/cart.controller"
 
-
-router.get("/",verifyTokenAndAdmin, CartController.getAllCart)
+router.get("/", CartController.getAllCart)
 router.post("/:productId", CartController.addToCart)
 router.put("/:cartId", CartController.updateCart)
 router.get("/:userId", CartController.getCart)
