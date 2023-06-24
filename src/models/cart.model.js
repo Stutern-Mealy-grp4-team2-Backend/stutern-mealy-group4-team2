363d6cf96154561f19e0f614cad3472560cb6836
Iupac120 <100804858+Iupac120@users.noreply.github.com>
2023-06-24 21:05:57 +0100

    working on cart and order

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,28 +1,5 @@
-// export default function Cart (oldCart){
-//     this.item = oldCart.item;
-//     this.totalQuantity = oldCart.totalQuantity;
-//     this.totalPrice = oldCart.totalPrice;
-
-//     this.add = function(item,id){
-//         let storedItem = this.item[id]
-//         if(!storedItem){
-//             storedItem = this.item[id] = {item:item,quantity:0,price:0}
-//         }
-//         storedItem.quantity++;
-//         storedItem.price = storedItem.item.price*storedItem.price;
-//         this.totalQuantity++;
-//         this.totalPrice += storedItem.price
-//     }
-//     this.generateArray = function(){
-//         let arr = []
-//         for (const id in this.item){
-//             arr.push(this.item[id])
-//         }
-//         return arr
-//     }
-// }
 export default function Cart(oldCart) {
-  this.items = oldCart.items || {}; // Initialize items if it's undefined in oldCart
+  this.items = oldCart.items || {};
   this.totalQuantity = oldCart.totalQuantity || 0;
   this.totalPrice = oldCart.totalPrice || 0;
 
@@ -32,24 +9,49 @@ export default function Cart(oldCart) {
       storedItem = this.items[id] = { item: item, quantity: 0, price: 0 };
     }
     storedItem.quantity++;
-    storedItem.price = storedItem.item.price * storedItem.quantity;
+
+    const productTotalPrice = item.discount > 0 ? item.price - (item.price * (item.discount / 100)) : item.price;
+    storedItem.price = productTotalPrice * storedItem.quantity;
+
     this.totalQuantity++;
-    this.totalPrice += storedItem.item.price;
+    this.totalPrice += productTotalPrice;
   };
-  this.reduceByOne = function(id){
-    this.items[id].quantity--;
-    this.items[id].price -= this.items[id].item.price;
-    this.totalQuantity--;
-    this.totalPrice -= this.items[id].item.price
-    if(this.item[id].item.quantity <= 0){
-      delete this.items[id]
+
+  this.addShippingFee = function (shippingFee) {
+    const shippingId = 'shipping'; // Use a unique ID for the shipping fee item
+    if (!this.items[shippingId]) {
+      this.items[shippingId] = { item: { name: 'Shipping Fee', price: shippingFee }, quantity: 1, price: shippingFee };
+      this.totalQuantity++;
+      this.totalPrice += shippingFee;
     }
   };
-  this.removeItem = function(id){
-    this.totalQuantity -= this.items[id].quantity ;
-    this.totalPrice -= this.items[id].price
-    delete this.items[id]
-  }
+
+  this.reduceByOne = function (id) {
+    const storedItem = this.items[id];
+    if (storedItem) {
+      storedItem.quantity--;
+
+      const productTotalPrice = storedItem.item.discount > 0 ? storedItem.item.price - (storedItem.item.price * (storedItem.item.discount / 100)) : storedItem.item.price;
+      storedItem.price = productTotalPrice * storedItem.quantity;
+
+      this.totalQuantity--;
+      this.totalPrice -= productTotalPrice;
+
+      if (storedItem.quantity <= 0) {
+        delete this.items[id];
+      }
+    }
+  };
+
+  this.removeItem = function (id) {
+    const storedItem = this.items[id];
+    if (storedItem) {
+      this.totalQuantity -= storedItem.quantity;
+      this.totalPrice -= storedItem.price;
+      delete this.items[id];
+    }
+  };
+
   this.generateArray = function () {
     let arr = [];
     for (const id in this.items) {
