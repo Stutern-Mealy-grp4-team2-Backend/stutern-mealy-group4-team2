fae3f60ccf7c115af9d158f6b03bec18ed82b920
Iupac120 <100804858+Iupac120@users.noreply.github.com>
2023-06-29 17:14:26 +0100

    discount updated

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -58,12 +58,12 @@ export default function Cart(oldCart) {
     const discount = await Discount.findOne(couponId)
     if(!discount) throw new UnAuthorizedError("You dont have a coupon")
     if(discount.couponValid > Date.now()) throw new BadUserRequestError("Coupon has expired")
-    if(discount.user === req.user.userId){
+    if(discount.user === req.user._id){
       throw new UnAuthorizedError("You have used this coupon")
     }else{
       this.totalQuantity;
       this.totalPrice -= discount.couponValue
-      discount.user = req.user.userId
+      discount.user = req.user._id
     }
     await discount.save()
   }
